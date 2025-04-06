c2fcfefb5a36e6b921c634800572ae549f721edf
Mahdi <mklhr@gmail.com>
2025-04-06 22:06:57 +0330

    timer up commit

--- src/timer.js
+++ src/timer.js
@@ -8,44 +8,53 @@ class Timer extends React.Component {
     constructor() {
         super()
         this.state = {
-            time: 10
+            hours: 0,
+            minutes: 0,
+            seconds: 0
         }
     }
 
-    interval() {
-        this.setState({
-            time: this.state.time - 1
-        })
+    startInterval = () => {
+        intervalTimer = setInterval(() => {
+            this.setState({
+                seconds: this.state.seconds + 1
+            })
+
+            if (this.state.seconds == 60) {
+                this.setState({
+                    seconds: 0,
+                    minutes: this.state.minutes + 1
+                })
+            }
+
+            if (this.state.minutes == 60) {
+                this.setState({
+                    minutes: 0,
+                    hours: this.state.hours + 1
+                })
+            }
+        }, 1000)
     }
 
-    componentDidMount() {
-        intervalTimer = setInterval(() => this.interval(), 1000)
-    }
-
-    componentDidUpdate() {
-        if (this.state.time == 0) {
-            this.stop()
-        }
-    }
-
-    start() {
-        this.stop()
-        intervalTimer = setInterval(() => this.interval(), 1000)
-    }
-
-    stop() {
+    stopInterval = () => {
         clearInterval(intervalTimer)
     }
 
     render() {
+        let h = this.state.hours
+        let m = this.state.minutes
+        let s = this.state.seconds
+
         return (
             <>
-                <h2 className='timer'>
-                    it is {this.state.time}
+                <h2 className="timer">
+                    {`${h > 9 ? h : "0" + h} : ${m > 9 ? m : "0" + m} : ${s > 9 ? s : "0" + s}`}
                 </h2>
-                <button onClick={() => this.start()}>start</button>
-                <button onClick={() => this.stop()}>stop</button>
-                <button onClick={this.props.handleChangeTitle}>change title</button>
+                <div className='box_btn'>
+                    <button className='btn btn-green' onClick={this.startInterval}>start</button>
+                    <button className='btn btn-red'>stop</button>
+                    <button className='btn btn-silver'>reset</button>
+                </div>
             </>
         )
     }
